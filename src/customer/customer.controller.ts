9ce1508a89b30cf09e3f847a8339c0b42ac93019
Yamile Lessa <yamile.fernandez@willbank.com.br>
2022-11-22 14:00:25 -0300

    adds firestore database configuration

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,33 +1,46 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Database } from 'src/database/database.interface';
 import { CustomerService } from './customer.service';
 
+
 @Controller('customer')
+
 export class CustomerController {
     constructor(private customerService: CustomerService) {}
 
+    @Get()
+        async getCustomers(){
+        return this.customerService.getAll()
+    }
+
+    @Get(':cpf')
+        async getCustomer(@Param('cpf') cpf:string){
+        return this.customerService.getCustomerById(cpf)
+    }
+
     @Post()
-    create(@Body() customer:any){
+    create(@Body() customer:Database){
         return this.customerService.createNewCustomer(customer)
     }
     
-    @Get()
-    getCustomers(){
-        return this.customerService.getAllCustomers()
-    }
+    // @Get()
+    // getCustomers(){
+    //     return this.customerService.getAllCustomers()
+    // }
 
-    @Get(':id')
-    getCustomer(@Param('id') id:number){
-        return this.customerService.getCustomerById(Number(id))
-    }
+    // @Get(':id')
+    // getCustomer(@Param('id') id:number){
+    //     return this.customerService.getCustomerById(Number(id))
+    // }
 
-    @Put(':id')
-    update(@Param('id') id:number, @Body() customer:any){
-        return this.customerService.updateACustomer(Number(id), customer)
-    }
+    // @Put(':id')
+    // update(@Param('id') id:number, @Body() customer:any){
+    //     return this.customerService.updateACustomer(Number(id), customer)
+    // }
 
-    @Delete(':id')
-    delete(@Param('id') id:number){
-        return this.customerService.deleteACustomer(Number(id))
-    }
+    // @Delete(':id')
+    // delete(@Param('id') id:number){
+    //     return this.customerService.deleteACustomer(Number(id))
+    // }
 
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,70 +1,84 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
 
-const customers = [{
-    id: 1,
-    name: 'Téo',
-    email: 'teo@email.com',
-    cellNumber: '(11)12312-1212'
-},{
-    id: 2,
-    name: 'Lyon',
-    email: 'lyon@email.com',
-    cellNumber: '(21)23423-2323'
-},{
-    id: 3,
-    name: 'Luna',
-    email: 'luna@email.com',
-    cellNumber: '(31)34534-3434'
-},{
-    id: 4,
-    name: 'Lelo',
-    email: 'lelo@email.com',
-    cellNumber: '(41)45645-4545'
-}]
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { FirestoreDatabase } from '../database/firestore-database';
+import { Database } from 'src/database/database.interface';
+
 
 @Injectable()
 export class CustomerService {
-    
-    getAllCustomers(){
-        return customers
+  constructor(private readonly firestore: FirestoreDatabase) {}
+
+    async getAll(): Promise<Database> {
+        const result = await this.firestore.getAllData('leads');
+        return result;
     }
-    
-    getCustomerById(id:number){
-        const ourCustomer = customers.find(customer => customer.id === id)
+
+    async getCustomerById(cpf:string){
+        const result = await this.firestore.getAllData('leads');
+        const ourCustomer = result.find((customer: { customer: Database; cpf: string}) => customer.cpf === cpf)
         if(!ourCustomer){
-            throw new NotFoundException('O cliente buscado não existe')
+            throw new NotFoundException ('O cliente buscado não existe')
         }
-        return ourCustomer 
+        return ourCustomer
     }
 
-    createNewCustomer(customer:any){
-        if(!customer){
-            throw new NotFoundException('As informações do cliente não foram definidas.')
-        }
-        customers.push(customer)
-        return customers
+    async createNewCustomer (object:Database): Promise<Database> {
+        const result = await this.firestore.getAllData('leads');
+        result.push(object)
+        console.log('Cadastrado com sucesso!')
+        return result
     }
 
-    updateACustomer(id:number, customer:any){
-        let customerToUpdate = customers.find(customer => customer.id === id)
-        if(!customerToUpdate){
-            throw new NotFoundException('O cliente a ser atualizado não existe')
-        }
-        if(!customer){
-            throw new NotFoundException('As informações do cliente não foram definidas.')
-        }
-        for(let i = 0; i < customers.length; i++){
-            if(customers[i].id === id){
-                customers[i].name = customer.name
-                customers[i].email = customer.email
-                customers[i].cellNumber = customer.cellNumber
-                return customers
-            }
-        }
-    }
-
-    deleteACustomer(id:number){
-
-    }
-
+//   async addLead(object: any): Promise<any> {
+//     const result = await this.firestore.add('leads', object);
+//     return 'Cadastrado com sucesso!!';
+//   }
 }
+
+
+// @Injectable()
+// export class CustomerService {
+    
+//     getAllCustomers(){
+//         return customers
+//     }
+    
+//     getCustomerById(id:number){
+//         const ourCustomer = customers.find(customer => customer.id === id)
+//         if(!ourCustomer){
+//             throw new NotFoundException('O cliente buscado não existe')
+//         }
+//         return ourCustomer 
+//     }
+
+//     createNewCustomer(customer:any){
+//         if(!customer){
+//             throw new NotFoundException('As informações do cliente não foram definidas.')
+//         }
+//         customers.push(customer)
+//         return customers
+//     }
+
+//     updateACustomer(id:number, customer:any){
+//         let customerToUpdate = customers.find(customer => customer.id === id)
+//         if(!customerToUpdate){
+//             throw new NotFoundException('O cliente a ser atualizado não existe')
+//         }
+//         if(!customer){
+//             throw new NotFoundException('As informações do cliente não foram definidas.')
+//         }
+//         for(let i = 0; i < customers.length; i++){
+//             if(customers[i].id === id){
+//                 customers[i].name = customer.name
+//                 customers[i].email = customer.email
+//                 customers[i].cellNumber = customer.cellNumber
+//                 return customers
+//             }
+//         }
+//     }
+
+//     deleteACustomer(id:number){
+
+//     }
+
+//}
